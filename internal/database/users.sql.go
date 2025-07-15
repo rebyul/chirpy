084f677d1bf0e447cca2f21e3d262a49877756d4
rebyul <rebyul@gmail.com>
2025-07-15 22:02:46 +1200

    The default value of hashed_password should be "unset"

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -7,23 +7,30 @@ package database
 
 import (
 	"context"
+	"database/sql"
 )
 
 const createUser = `-- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email)
-    VALUES (gen_random_uuid (), now(), now(), $1)
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
+    VALUES (gen_random_uuid (), now(), now(), $1, $2)
 RETURNING
-    id, created_at, updated_at, email
+    id, created_at, updated_at, email, hashed_password
 `
 
-func (q *Queries) CreateUser(ctx context.Context, email string) (User, error) {
-	row := q.db.QueryRowContext(ctx, createUser, email)
+type CreateUserParams struct {
+	Email          string
+	HashedPassword sql.NullString
+}
+
+func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, createUser, arg.Email, arg.HashedPassword)
 	var i User
 	err := row.Scan(
 		&i.ID,
 		&i.CreatedAt,
 		&i.UpdatedAt,
 		&i.Email,
+		&i.HashedPassword,
 	)
 	return i, err
 }
