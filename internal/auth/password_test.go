53634eb1903586c60cc813abd9fa6fdd78a02dd8
rebyul <rebyul@gmail.com>
2025-07-15 23:47:59 +1200

    Rewrite test to check checkpwhash

--- internal/auth/password_test.go
+++ internal/auth/password_test.go
@@ -7,33 +7,54 @@ import (
 )
 
 func TestHashPassword(t *testing.T) {
+	// First, we need to create some hashed passwords for testing
+	password1 := "correctPassword123!"
+	password2 := "anotherPassword456!"
+	hash1, _ := auth.HashPassword(password1)
+	hash2, _ := auth.HashPassword(password2)
 	tests := []struct {
 		name string // description of this test case
 		// Named input parameters for target function.
 		password string
+		hash     string
 		wantErr  bool
 	}{
 		{
-			name:     "Hash and check password hash",
-			password: "asdfasdf",
+			name:     "Correct password",
+			password: password1,
+			hash:     hash1,
 			wantErr:  false,
+		},
+		{
+			name:     "Incorrect password",
+			password: "wrongPassword",
+			hash:     hash1,
+			wantErr:  true,
+		},
+		{
+			name:     "Password doesn't match different hash",
+			password: password1,
+			hash:     hash2,
+			wantErr:  true,
+		},
+		{
+			name:     "Empty password",
+			password: "",
+			hash:     hash1,
+			wantErr:  true,
+		},
+		{
+			name:     "Invalid hash",
+			password: password1,
+			hash:     "invalidhash",
+			wantErr:  true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, gotErr := auth.HashPassword(tt.password)
-			if gotErr != nil {
-				if !tt.wantErr {
-					t.Errorf("HashPassword() failed: %v", gotErr)
-				}
-				return
-			}
-			if tt.wantErr {
-				t.Fatal("HashPassword() succeeded unexpectedly")
-			}
-			failedPwCheck := auth.CheckPasswordHash(tt.password, got)
-			if failedPwCheck != nil {
-				t.Errorf("HashPassword() = %v, failed CheckPasswordHash(): %v", got, failedPwCheck)
+			failedPwCheck := auth.CheckPasswordHash(tt.password, tt.hash)
+			if (failedPwCheck != nil) != tt.wantErr {
+				t.Errorf("CheckPasswordHash() err = %v, wantErr %v", failedPwCheck, tt.wantErr)
 			}
 		})
 	}
