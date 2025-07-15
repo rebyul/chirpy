f700254d256fa5e64fe6f87f81f5e7062f6c2975
rebyul <rebyul@gmail.com>
2025-07-15 23:48:34 +1200

    Space

--- internal/auth/password_test.go
+++ internal/auth/password_test.go
@@ -50,6 +50,7 @@ func TestHashPassword(t *testing.T) {
 			wantErr:  true,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			failedPwCheck := auth.CheckPasswordHash(tt.password, tt.hash)
